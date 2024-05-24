06358a7dc5a27178e3e40d57c82fb5f4e284a9b2
SviatoslavJ <130385673+SviatoslavJ@users.noreply.github.com>
2024-05-24 22:21:11 +0200

    Update4 final.py

--- final.py
+++ final.py
@@ -7,7 +7,7 @@ import yaml
 def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("input_file", help="The input file containing JSON or YAML data")
-    parser.add_argument("output_file", help="The output file to save the JSON data")
+    parser.add_argument("output_file", help="The output file to save the data in JSON or YAML format")
     return parser.parse_args()
 
 def loading_json(input_file):
@@ -39,25 +39,37 @@ def loading_yaml(input_file):
 def save_json(obj, output_file):
     with open(output_file, "w") as file_js:
         json.dump(obj, file_js, indent=4)
-    
+
+def save_yaml(obj, output_file):
+    with open(output_file, "w") as file_ym:
+        yaml.dump(obj, file_ym, default_flow_style=False)
+
 def main():
     args = parse_arguments()
     input_file = args.input_file
     output_file = args.output_file
 
-    file_extension = os.path.splitext(input_file)[1].lower()
+    input_extension = os.path.splitext(input_file)[1].lower()
+    output_extension = os.path.splitext(output_file)[1].lower()
     
-    if file_extension == ".json":
+    if input_extension == ".json":
         obj = loading_json(input_file)
-    elif file_extension == ".yml" or file_extension == ".yaml":
+    elif input_extension == ".yml" or input_extension == ".yaml":
         obj = loading_yaml(input_file)
     else:
         print("Input file is neither JSON nor YAML file")
         sys.exit(1)
 
-    save_json(obj, output_file)
+    if output_extension == ".json":
+        save_json(obj, output_file)
+    elif output_extension == ".yml" or output_extension == ".yaml":
+        save_yaml(obj, output_file)
+    else:
+        print("Output file extension is not supported (use .json or .yml/.yaml)")
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
+
 
 
